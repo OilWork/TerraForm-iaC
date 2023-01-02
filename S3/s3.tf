3da47e2e22edd5269a2e2a770c96518154231d7a
OilWork <gmlehs0101@gamil.com>
2023-01-02 13:58:09 +0900

    s3

---
+++ S3/s3.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "main" {
+ bucket = "devops-study-don"
+
+ tags ={
+ Name = "devops-study-don"
+ }
+}
--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -44,12 +44,30 @@ resource "aws_eip" "nat" {
  }
 }
 
-resource "aws_nat_gateway" "nat_gateway" {
- allocation_id = aws_eip.nat.id
-
- subnet_id = aws_subnet.public_subnet.id
-
+resource "aws_route_table" "public" {
+ vpc_id = aws_vpc.main.id
+  
  tags = {
-  Name = "terraformdon_NATGW"
+  Name = "terraformdon-rt-public"
  }
 }
+
+resource "aws_route_table_association" "route_table_association_public" {
+ subnet_id = aws_subnet.public_subnet.id
+ route_table_id = aws_route_table.public.id
+} 
+
+resource "aws_route_table" "private" {
+ vpc_id = aws_vpc.main.id
+
+ tags = {
+  Name = "terraformdon-rt-private"
+ }
+}
+
+resource "aws_route_table_association" "route_table_association_private" {
+ subnet_id = aws_subnet.private_subnet.id
+ route_table_id = aws_route_table.private.id
+}
+
+
